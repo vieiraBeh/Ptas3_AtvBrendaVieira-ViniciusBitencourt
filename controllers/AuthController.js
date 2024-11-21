9b489782f52d52f9eb9afa44a60aa1e45ea23d9c
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-11-20 20:46:25 -0400

    commit
    teste postman

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,7 +2,21 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class AuthController{
-    static async cadastro(req, res){}
+    static async cadastro(req, res){
+        const {nome, email, password, tipo} = req.body;
+
+        if(!nome || nome.lenght < 6){
+            return res.json({
+                erro : true,
+                mensagem: "Nome deve ter pelo menos 6 caracteres."
+            });
+        }
+        return res.json({
+            erro: false, 
+            mensagem: "Usuário cadastrado com sucesso!",
+            token: "3klçjdfusda9f8as341"
+        });
+    }
 
     static async login(req, res){}
 }
