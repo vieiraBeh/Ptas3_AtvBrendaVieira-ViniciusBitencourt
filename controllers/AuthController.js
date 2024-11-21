6b7b6c80af7bbc53074ffddcddcffe6b3276573a
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-11-20 21:59:19 -0400

    Operação de cadastro

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -11,11 +11,48 @@ class AuthController{
                 mensagem: "Nome deve ter pelo menos 6 caracteres."
             });
         }
-        return res.json({
-            erro: false, 
-            mensagem: "Usuário cadastrado com sucesso!",
-            token: "3klçjdfusda9f8as341"
+
+        if(!email || email.lenght < 10){
+            return res.json({
+                erro : true,
+                mensagem: "O email deve ter pelo menos 10 caracteres."
+            });
+        }
+
+        if(!password || password.lenght < 8){
+            return res.json({
+                erro : true,
+                mensagem: "A senha deve ter pelo menos 8 caracteres."
+            });
+        }
+
+        const existe = await prisma.usuario.count({
+            where:{
+                email: email,
+            },
         });
+
+        if(esxite != 0){
+            return res.json({
+                erro:true,
+                mensagem: "Email já cadastrado."
+            });
+        }
+
+        await prisma.usuario.create({
+            data:{
+                nome: nome,
+                email: email,
+                password: password,
+                tipo: "cliente",
+            },
+        })
+
+        //return res.json({
+            //erro: false, 
+            //mensagem: "Usuário cadastrado com sucesso!",
+            //token: "3klçjdfusda9f8as341"
+        //});
     }
 
     static async login(req, res){}
