806d93ca569b387891536d26d128770fee82e596
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-11-27 09:23:27 -0400

    commit
    erro

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const prisma = require("./prisma/prismaClient");
 
 const express = require("express");
+const cors = require("cors")
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
