80db74065a8994338366fb957115c563d9360e11
vieiraBeh <brendanascimentovieirah@gmail.com>
2025-02-03 20:52:17 -0400

    commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,6 +4,7 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 class AuthController{
+    
     static async cadastro(req, res){
         const {nome, email, password, tipo} = req.body;
 
